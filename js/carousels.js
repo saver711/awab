1eb203bc077f6c857c22eebe2fe0c96b2d3d8a5d
unknown <ahwork711@gmail.com>
2022-03-24 17:58:57 +0200

    BaBy

---
+++ js/carousels.js
@@ -0,0 +1,455 @@
+(function($) { "use strict";
+
+
+	
+//Home Carousel
+
+
+
+$(document).ready(function() {
+ 
+  var sync1 = $("#sync3");
+  var sync2 = $("#sync4");
+ 
+  sync1.owlCarousel({
+    singleItem : true,
+    slideSpeed : 1000,
+    transitionStyle : "backSlide",
+    navigation: false,
+    pagination:false,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+      afterInit : progressBar,
+      afterMove : moved,
+      startDragging : pauseOnDragging
+  });
+
+
+  var time = 5; // time in seconds
+ 
+  var $progressBar,
+      $bar, 
+      $elem, 
+      isPause, 
+      tick,
+      percentTime;
+
+ 
+    //Init progressBar where elem is $("#owl-demo")
+    function progressBar(elem){
+      $elem = elem;
+      //build progress bar elements
+      buildProgressBar();
+      //start counting
+      start();
+    }
+ 
+    //create div#progressBar and div#bar then prepend to $("#owl-demo")
+    function buildProgressBar(){
+      $progressBar = $("<div>",{
+        id:"progressBar"
+      });
+      $bar = $("<div>",{
+        id:"bar"
+      });
+      $progressBar.append($bar).prependTo($elem);
+    }
+ 
+    function start() {
+      //reset timer
+      percentTime = 0;
+      isPause = false;
+      //run interval every 0.01 second
+      tick = setInterval(interval, 10);
+    };
+ 
+    function interval() {
+      if(isPause === false){
+        percentTime += 1 / time;
+        $bar.css({
+           width: percentTime+"%"
+         });
+        //if percentTime is equal or greater than 100
+        if(percentTime >= 100){
+          //slide to next item 
+          $elem.trigger('owl.next')
+        }
+      }
+    }
+ 
+    //pause while dragging 
+    function pauseOnDragging(){
+      isPause = true;
+    }
+ 
+    //moved callback
+    function moved(){
+      //clear interval
+      clearTimeout(tick);
+      //start again
+      start();
+    }
+
+
+
+
+  
+  sync2.owlCarousel({
+    items : 6,
+    itemsDesktop      : [1199,6],
+    itemsDesktopSmall     : [979,6],
+    itemsTablet       : [768,6],
+    itemsMobile       : [479,6],
+    pagination:false,
+    responsiveRefreshRate : 100,
+    afterInit : function(el){
+      el.find(".owl-item").eq(0).addClass("synced");
+    }
+  });
+ 
+  function syncPosition(el){
+    var current = this.currentItem;
+    $("#sync4")
+      .find(".owl-item")
+      .removeClass("synced")
+      .eq(current)
+      .addClass("synced")
+    if($("#sync4").data("owlCarousel") !== undefined){
+      center(current)
+    }
+  }
+ 
+  $("#sync4").on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).data("owlItem");
+    sync1.trigger("owl.goTo",number);
+  });
+ 
+  function center(number){
+    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+    var num = number;
+    var found = false;
+    for(var i in sync2visible){
+      if(num === sync2visible[i]){
+        var found = true;
+      }
+    }
+ 
+    if(found===false){
+      if(num>sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", num - sync2visible.length+2)
+      }else{
+        if(num - 1 === -1){
+          num = 0;
+        }
+        sync2.trigger("owl.goTo", num);
+      }
+    } else if(num === sync2visible[sync2visible.length-1]){
+      sync2.trigger("owl.goTo", sync2visible[1])
+    } else if(num === sync2visible[0]){
+      sync2.trigger("owl.goTo", num-1)
+    }
+    
+  }
+ 
+});	
+
+
+
+
+
+//Twit Carousel
+
+
+
+$(document).ready(function() {
+ 
+  var sync1 = $("#sync1");
+  var sync2 = $("#sync2");
+ 
+  sync1.owlCarousel({
+    singleItem : true,
+    slideSpeed : 1000,
+	autoPlay: 4000,
+    transitionStyle : "backSlide",
+    navigation: false,
+    pagination:false,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+  });
+
+
+
+  
+  sync2.owlCarousel({
+    items : 3,
+    itemsDesktop      : [1199,3],
+    itemsDesktopSmall     : [979,3],
+    itemsTablet       : [768,3],
+    itemsMobile       : [479,3],
+    pagination:false,
+    responsiveRefreshRate : 100,
+    afterInit : function(el){
+      el.find(".owl-item").eq(0).addClass("synced");
+    }
+  });
+ 
+  function syncPosition(el){
+    var current = this.currentItem;
+    $("#sync2")
+      .find(".owl-item")
+      .removeClass("synced")
+      .eq(current)
+      .addClass("synced")
+    if($("#sync2").data("owlCarousel") !== undefined){
+      center(current)
+    }
+  }
+ 
+  $("#sync2").on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).data("owlItem");
+    sync1.trigger("owl.goTo",number);
+  });
+ 
+  function center(number){
+    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+    var num = number;
+    var found = false;
+    for(var i in sync2visible){
+      if(num === sync2visible[i]){
+        var found = true;
+      }
+    }
+ 
+    if(found===false){
+      if(num>sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", num - sync2visible.length+2)
+      }else{
+        if(num - 1 === -1){
+          num = 0;
+        }
+        sync2.trigger("owl.goTo", num);
+      }
+    } else if(num === sync2visible[sync2visible.length-1]){
+      sync2.trigger("owl.goTo", sync2visible[1])
+    } else if(num === sync2visible[0]){
+      sync2.trigger("owl.goTo", num-1)
+    }
+    
+  }
+ 
+});	
+
+
+
+
+ // Logos Carousel
+
+
+$(document).ready(function() {
+ 
+  var owl = $("#owl-logos");
+ 
+  owl.owlCarousel({
+     
+      itemsCustom : [
+        [0, 2],
+        [450, 2],
+        [600, 2],
+        [700, 3],
+        [1000, 4],
+        [1200, 4],
+        [1400, 4],
+        [1600, 4]
+      ],
+    navigation : false,
+    pagination: false,
+	autoPlay: 2000
+ 
+  });
+ 
+});
+
+	
+//Services Carousel
+
+
+
+$(document).ready(function() {
+ 
+  var sync1 = $("#sync5");
+  var sync2 = $("#sync6");
+ 
+  sync1.owlCarousel({
+    singleItem : true,
+    slideSpeed : 1000,
+	autoPlay: 4000,
+    transitionStyle : "backSlide",
+    navigation: false,
+    pagination:false,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+  });
+
+
+
+  
+  sync2.owlCarousel({
+    items : 2,
+    itemsDesktop      : [1199,2],
+    itemsDesktopSmall     : [979,2],
+    itemsTablet       : [768,2],
+    itemsMobile       : [479,2],
+    pagination:false,
+    responsiveRefreshRate : 100,
+    afterInit : function(el){
+      el.find(".owl-item").eq(0).addClass("synced");
+    }
+  });
+ 
+  function syncPosition(el){
+    var current = this.currentItem;
+    $("#sync6")
+      .find(".owl-item")
+      .removeClass("synced")
+      .eq(current)
+      .addClass("synced")
+    if($("#sync6").data("owlCarousel") !== undefined){
+      center(current)
+    }
+  }
+ 
+  $("#sync6").on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).data("owlItem");
+    sync1.trigger("owl.goTo",number);
+  });
+ 
+  function center(number){
+    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+    var num = number;
+    var found = false;
+    for(var i in sync2visible){
+      if(num === sync2visible[i]){
+        var found = true;
+      }
+    }
+ 
+    if(found===false){
+      if(num>sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", num - sync2visible.length+2)
+      }else{
+        if(num - 1 === -1){
+          num = 0;
+        }
+        sync2.trigger("owl.goTo", num);
+      }
+    } else if(num === sync2visible[sync2visible.length-1]){
+      sync2.trigger("owl.goTo", sync2visible[1])
+    } else if(num === sync2visible[0]){
+      sync2.trigger("owl.goTo", num-1)
+    }
+    
+  }
+ 
+});		
+	
+	
+//Contact Carousel
+
+
+
+$(document).ready(function() {
+ 
+  var sync1 = $("#sync7");
+  var sync2 = $("#sync8");
+ 
+  sync1.owlCarousel({
+    singleItem : true,
+    slideSpeed : 1000,
+    transitionStyle : "backSlide",
+    navigation: false,
+    pagination:false,
+	autoHeight: true,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+  });
+
+
+
+  
+  sync2.owlCarousel({
+    items : 3,
+    itemsDesktop      : [1199,3],
+    itemsDesktopSmall     : [979,3],
+    itemsTablet       : [768,3],
+    itemsMobile       : [479,3],
+    pagination:false,
+    responsiveRefreshRate : 100,
+    afterInit : function(el){
+      el.find(".owl-item").eq(0).addClass("synced");
+    }
+  });
+ 
+  function syncPosition(el){
+    var current = this.currentItem;
+    $("#sync8")
+      .find(".owl-item")
+      .removeClass("synced")
+      .eq(current)
+      .addClass("synced")
+    if($("#sync8").data("owlCarousel") !== undefined){
+      center(current)
+    }
+  }
+ 
+  $("#sync8").on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).data("owlItem");
+    sync1.trigger("owl.goTo",number);
+  });
+ 
+  function center(number){
+    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+    var num = number;
+    var found = false;
+    for(var i in sync2visible){
+      if(num === sync2visible[i]){
+        var found = true;
+      }
+    }
+ 
+    if(found===false){
+      if(num>sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", num - sync2visible.length+2)
+      }else{
+        if(num - 1 === -1){
+          num = 0;
+        }
+        sync2.trigger("owl.goTo", num);
+      }
+    } else if(num === sync2visible[sync2visible.length-1]){
+      sync2.trigger("owl.goTo", sync2visible[1])
+    } else if(num === sync2visible[0]){
+      sync2.trigger("owl.goTo", num-1)
+    }
+    
+  }
+ 
+});		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })(jQuery); 
